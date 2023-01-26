e71f548de97518a3a13cd6fa9a6f7ca26a6c98c7
Nazar Mosula <nmosula@gmail.com>
2023-01-26 18:49:27 +0200

    create render-by-genre.js

---
+++ src/main.js/render-by-genre.js
@@ -0,0 +1,37 @@
+import { apiTheMovies} from './render-all-collection';
+	
+	import allCollection from '../templates/all-collection-movies.hbs';
+	
+
+	const genreList = document.querySelector('.genre-list');
+	genreList.addEventListener('click', onCallByGanre);
+	
+
+	function onCallByGanre(e) {
+	  if (e.currentTarget === e.target) {
+	    return;
+	  }
+	  apiTheMoviesdb.resetPage();
+	  apiThemoviedb.genreId = e.target.dataset.id;
+	  apiThemoviedb.fetchFilmsByGenre(apiThemoviedb.genreId).then(onRenderbyGenre);
+	}
+	
+
+	function onRenderbyGenre(owner) {
+	  const markupGenre = allCollection(owner);
+	  refs.gallery.innerHTML = markupGenre;
+}
+    
+
+
+    const btnShowGenre = document.querySelector(".js-show-genres");
+    btnShowGenre.addEventListener('click', onShowGenres);
+
+    function onShowGenres() {
+        var x = document.querySelector(".genre-list");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
