0da2264fd4baac4322aa3a95f44614ca55105392
LuckuGuy <alexveselovsky95@gmail.com>
2023-01-26 08:16:31 +0200

    modal-film upd
    деякі зміни в стилях та спроби добавити фільм в бібліотеку Watched при натисканні на клавішу

--- src/main.js/render-by-id.js
+++ src/main.js/render-by-id.js
@@ -2,6 +2,11 @@ import { ApiTheMovie } from './fetch-class';
 import modalFunction from '../templates/modal-movies.hbs'
 import *as basicLightBox  from "basiclightbox";
 import 'basiclightbox/dist/basicLightbox.min.css';
+import {
+    getWatchedList,
+    renderMoviesinWatchedLibrary,
+    setMovieToLocalStorage
+} from './watched-library-temp';
 
 const apiTheMovies = new ApiTheMovie();
 const gallery = document.querySelector('.gallery');
@@ -14,7 +19,7 @@ function onCardClick(event) {
     }
     
     const currentId = event.target.closest('.movie-card__item').dataset.id;
-    apiTheMovies.setMovieId(currentId);
+    apiTheMovies.setMovieId(currentId)
     apiTheMovies.fetchById(currentId)
     .then(onOpenCard);
 }
@@ -24,4 +29,28 @@ function onOpenCard(data) {
     const markUp = modalFunction(data);
     const instance = basicLightBox.create(markUp);
     instance.show();
+    document.body.classList.add('stop-fon');
+    //==  міняємо ADD TO WATCHED на REMOVE FROM WATCHED
+    let watchedList = getWatchedList();
+    const modalLibrarryBtn = document.querySelector('.modal-btn__watched');
+  if (!watchedList.find(film => film.id === data.id)) {
+    modalLibrarryBtn.textContent = 'Add to watched';
+  } else {
+    modalLibrarryBtn.textContent = 'Remove from watched';
+  }
+
+//== закриття бекдропа ESC
+window.addEventListener('keydown', onKeydownEsc);
+function onKeydownEsc(event) {
+    if (event.code === 'Escape') {
+        instance.close()
+        document.body.classList.remove('stop-fon');
+    }
+    };    
+
+    //== добавити карточку в WATCHED
+    modalLibrarryBtn.addEventListener('click', renderMoviesinWatchedLibrary);
 }
+
+
+
