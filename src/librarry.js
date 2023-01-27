ae2ce6572c12c6d80070929dbd24f222e5213084
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-01-27 17:39:37 +0200

    add switcher and style theme

--- src/librarry.js
+++ src/librarry.js
@@ -4,3 +4,4 @@ import './main.js/google-translate';
 import './main.js/render-favorites-movie';
 import './main.js/fetch-class';
 import './main.js/scroll-up-btn';
+import './main.js/switcher';
---
+++ src/main.js/switcher.js
@@ -0,0 +1,25 @@
+const themeBtn = document.querySelector('.switcher-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme == 'dark') {
+  document.body.classList.add('toggle-theme');
+  document.getElementById('slider').checked = true;
+} else {
+  document.getElementById('slider').checked = false;
+}
+
+themeBtn.addEventListener('click', onToggleTheme);
+
+function onToggleTheme() {
+  document.body.classList.toggle('toggle-theme');
+
+  // Допустим, тема светлая
+  let theme = 'light';
+  // Если <body> содержит класс .dark-theme…
+  if (document.body.classList.contains('toggle-theme')) {
+    // …тогда делаем тему тёмной
+    theme = 'dark';
+  }
+  // После чего сохраняем выбор в localStorage
+  localStorage.setItem('theme', theme);
+}
