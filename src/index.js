3da925ef68fe8275c5acf2ea69b97f3653722a28
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-01-28 01:53:01 +0200

    add preloader

--- src/index.js
+++ src/index.js
@@ -12,3 +12,4 @@ import './main.js/watched-library-temp';
 import './main.js/scroll-up-btn';
 import './main.js/switcher';
 import './main.js/hidden-footer';
+import './main.js/preloader';
--- src/librarry.js
+++ src/librarry.js
@@ -6,3 +6,4 @@ import './main.js/fetch-class';
 import './main.js/scroll-up-btn';
 import './main.js/switcher';
 import './main.js/hidden-footer';
+import './main.js/preloader';
