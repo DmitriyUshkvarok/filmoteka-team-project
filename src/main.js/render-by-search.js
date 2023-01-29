ae67629b73c1d5782116301e5b7b452fa31800ba
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-01-29 18:41:19 +0200

    ...

--- src/main.js/render-by-search.js
+++ src/main.js/render-by-search.js
@@ -1,3 +1,43 @@
+// import allCollectionFunction from '../templates/all-collection-movies.hbs';
+// import { ApiTheMovie } from './fetch-class';
+// import 'lazysizes';
+// import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+// import { async } from '@firebase/util';
+
+// const apiTheMovies = new ApiTheMovie();
+// const form = document.querySelector('.header-form');
+// const input = document.querySelector('.header-input');
+// const warning = document.querySelector('.js-search');
+// const gallery = document.querySelector('.gallery');
+
+// form.addEventListener('submit', onSubmit);
+
+// async function onSubmit(e) {
+//   e.preventDefault();
+
+//   apiTheMovies.searchValue = input.value;
+//   const res = await apiTheMovies.fetchBySearch();
+
+//   if (res.total_results == 0) {
+//     warning.style.color = '#FF001B';
+//     warning.style.textAlign = 'center';
+//     warning.style.marginBottom = '10px';
+//     warning.removeAttribute('hidden');
+//     // warning.style.visibility = 'visible';
+//     setTimeout(() => {
+//       // warning.style.visibility = 'hidden';
+//       warning.setAttribute('hidden', '');
+//     }, 3000);
+//     return;
+//   }
+//   createMarkup(res);
+// }
+
+// function createMarkup(array) {
+//   const result = allCollectionFunction(array);
+//   gallery.innerHTML = result;
+// }
+
 import allCollectionFunction from '../templates/all-collection-movies.hbs';
 import { apiTheMovies } from './render-all-collection';
 import 'lazysizes';
@@ -7,12 +47,14 @@ import Notiflix from 'notiflix';
 const form = document.querySelector('.header-form');
 const warning = document.querySelector('.js-search');
 const gallery = document.querySelector('.gallery');
+const input = document.querySelector('.header-input');
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  apiTheMovies.searchValue = e.currentTarget.elements.searchQuery.value.trim();
+  //   apiTheMovies.searchValue = e.currentTarget.elements.searchQuery.value.trim();
+  apiTheMovies.searchValue = input.value.trim();
 
   if (!apiTheMovies.searchValue) {
     Notiflix.Notify.info('Enter the name movie');
